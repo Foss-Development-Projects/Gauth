cce32de5bc74d2c0a82f6eef7b3e238258dbc579
Foster <72973015+Foster0123@users.noreply.github.com>
2023-01-10 21:07:41 +0530

    Initial Commit 2

--- src/main.go
+++ src/main.go
@@ -5,7 +5,7 @@ import (
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -15,11 +15,14 @@ type Button struct {
 func main() {
 	a := app.New()
 	w := a.NewWindow("Gauth")
-	button := widget.NewButtonWithIcon("Update", theme.HomeIcon(), func() {
+	button := widget.NewButton("Update", func() {
 		log.Println("Updated")
 	})
+
+	button.Resize(fyne.NewSize(100, 20))
 	w.SetContent(widget.NewLabel("Gauth"))
-	w.SetContent(button)
-	w.Resize(fyne.NewSize(200, 200))
+	w.SetContent(container.NewCenter(button))
+	w.Resize(fyne.NewSize(600, 600))
+
 	w.ShowAndRun()
 }
